a35f93752a326dd13537115d2879f8ac5ea4f686
dimabogachov <dima1983bogachov@gmail.com>
2021-09-30 22:59:32 +0300

    Вывод средней температуры за сутки

---
+++ Lesson-2_Exercise-1/Lesson-2_Exercise-1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_2_Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Какая минимальная температура за сутки?");
+            string MinimumTempDay = Console.ReadLine();
+            float Min = Convert.ToInt32(MinimumTempDay);
+
+            Console.WriteLine("Какая максимальная температура за сутки?");
+            string MaximumTempDay = Console.ReadLine();
+            float Max = Convert.ToInt32(MaximumTempDay);
+
+            float Mean = (Max + Min) / 2;
+
+            Console.WriteLine($"Среднесуточная температура - {Mean} градусов Цельсия");
+
+        }
+    }
+}
